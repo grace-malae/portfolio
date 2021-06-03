2cd7e95c6a70a10ecee01a6bd4ef09872f38c023
grace-malae <grace.t.malae@gmail.com>
2021-06-04 01:58:13 +1200

    start to change buttons to link to's

--- client/components/Buttons/AboutMeButton.jsx
+++ client/components/Buttons/AboutMeButton.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function AboutMeButton () {
-  const history = useHistory()
-  function handleClick () {
-    history.push('/aboutme')
-  }
   return (
     <>
-      <button className='focus:outline-none hover:shadow-2xl py-3 px-4 self-center bg-transparent hover:text-pink-500 hover:bg-white ring-4 ring-white rounded-full focus:ring transform transition hover:scale-105 duration-300 ease-in-out' onClick={handleClick}>
-          About Me</button>
+      <Link to='/aboutme' className='hover:shadow-2xl py-3 px-4 self-center bg-transparent hover:text-pink-500 hover:bg-white ring-4 ring-white rounded-full focus:ring transform transition hover:scale-105 duration-300 ease-in-out'>
+          About Me</Link>
     </>
   )
 }
--- client/components/Buttons/ContactButton.jsx
+++ client/components/Buttons/ContactButton.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ContactButton () {
-  const history = useHistory()
-  function handleClick () {
-    history.push('/contact')
-  }
   return (
     <>
-      <button className='focus:outline-none hover:shadow-2xl py-3 px-4 self-center bg-transparent hover:text-pink-500 hover:bg-white ring-4 ring-white rounded-full focus:ring transform transition hover:scale-105 duration-300 ease-in-out' onClick={handleClick}>
-          Contact</button>
+      <Link to='/contact' className='focus:outline-none hover:shadow-2xl py-3 px-4 self-center bg-transparent hover:text-pink-500 hover:bg-white ring-4 ring-white rounded-full focus:ring transform transition hover:scale-105 duration-300 ease-in-out'>
+          Contact</Link>
     </>
   )
 }
