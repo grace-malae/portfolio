bdc94fdc529d1295f69d2ed24d97b83c6d98e2f6
grace-malae <grace.t.malae@gmail.com>
2021-06-09 23:23:05 +1200

    work page is responsive

--- client/components/Pages/Work.jsx
+++ client/components/Pages/Work.jsx
@@ -5,9 +5,9 @@ import GetInTouch from '../GetInTouch'
 export default function Work () {
   return (
     <>
-      <div className='flex flex-row self-center w-full pt-52 justify-center'>
-        <img className='self-center w-14 h-14' src='/images/BlackFranji.png' />
-        <h1 className="text-5xl font-bold leading-tight text-center text-black">
+      <div className='flex flex-row self-center w-full pt-32 md:pt-52 justify-center'>
+        <img className='self-center w-8 h-8 md:w-14 md:h-14' src='/images/BlackFranji.png' />
+        <h1 className="text-3xl md:text-5xl font-bold leading-tight text-center text-black">
           Work
         </h1>
       </div>
@@ -17,14 +17,18 @@ export default function Work () {
       <div className='flex flex-col w-full'>
         <div className="w-full md:w-2/3 p-6 flex flex-col mx-auto">
           <a href='https://tuohunga.herokuapp.com/#/' target='blank'>
-            <div className='flex md:flex-row rounded-lg bg-gray-100 m-6 shadow-xl hover:scale-105 duration-300 ease-in-out transform transition'>
-              <div className="h-48 w-3/4">
-                <h1 className="w-full p-6 font-bold text-xl text-gray-800">Tuohunga</h1>
+            <div className='flex flex-col md:flex-row rounded-lg bg-gray-100 m-6 shadow-xl hover:scale-105 duration-300 ease-in-out transform transition'>
+              <h1 className="w-full p-6 font-bold text-xl text-gray-800 text-center md:hidden">Tuohunga</h1>
+              <div className='w-full self-center md:hidden'>
+                <img className='p-4' src='/images/preview1.png'/>
+              </div>
+              <div className="h-full w-full text-justify md:w-3/4">
+                <h1 className="w-full p-6 font-bold text-2xl text-gray-800 hidden md:flex">Tuohunga</h1>
                 <p className="text-gray-800 text-base px-6 mb-5">
-                    Tuohunga is a web app created with the intention of helping Woman&apos;s Refuge NZ. It is intending to help clients get into safehouses faster.
+                    Tuohunga is a web app created with the intention of helping Woman&apos;s Refuge NZ. It is intending to help clients get into safehouses as quickly as possible.
                 </p>
               </div>
-              <div className='w-2/4 self-center'>
+              <div className='w-2/4 self-center hidden md:flex'>
                 <img className='p-4' src='/images/preview1.png'/>
               </div>
             </div>
@@ -33,14 +37,18 @@ export default function Work () {
 
         <div className="w-full md:w-2/3 p-6 flex flex-col mx-auto">
           <a href='https://tuohunga.herokuapp.com/#/' target='blank'>
-            <div className='flex md:flex-row rounded-lg bg-gray-100 m-6 shadow-xl hover:scale-105 duration-300 ease-in-out transform transition'>
-              <div className="overflow-hidden h-48 w-3/4">
-                <h1 className="w-full p-6 font-bold text-xl text-gray-800">Kupu</h1>
+            <div className='flex flex-col md:flex-row rounded-lg bg-gray-100 m-6 shadow-xl hover:scale-105 duration-300 ease-in-out transform transition'>
+              <h1 className="w-full p-6 font-bold text-xl text-gray-800 text-center md:hidden">Kupu</h1>
+              <div className='w-full self-center md:hidden'>
+                <img className='p-4' src='/images/preview2.png'/>
+              </div>
+              <div className="h-full w-full text-justify md:w-3/4">
+                <h1 className="w-full p-6 font-bold text-2xl text-gray-800 hidden md:flex">Kupu</h1>
                 <p className="text-gray-800 text-base px-6 mb-5">
-                Kupu is a web app that generates a Māori word of the day, with a phonetic pronunciation of the day&apos;s word, and the meaning.
+                Kupu is a web app that generates a Māori word of the day, with a phonetic pronunciation, and meaning.
                 </p>
               </div>
-              <div className='w-2/4 self-center'>
+              <div className='w-2/4 self-center hidden md:flex'>
                 <img className='p-4' src='/images/preview2.png'/>
               </div>
             </div>
